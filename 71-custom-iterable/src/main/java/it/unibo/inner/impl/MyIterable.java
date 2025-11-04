af28e4f37e722923a8844480b315d56732c4e4f9
Davide Raschi - davide.raschi@studio.unibo.it <davide.raschi@studio.unibo.it>
2025-11-04 15:13:05 +0100

    updated ex 71: using Array and not LL, added finals

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/MyIterable.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/MyIterable.java
@@ -2,73 +2,82 @@ package it.unibo.inner.impl;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
 
+/**
+ * {@inheritDoc}
+ */
 public class MyIterable<T> implements IterableWithPolicy<T> {
 
-    private List<T> data;
+    private final T[] data;
     private Predicate<T> pred;
 
-    @SuppressWarnings("unused")
-    private MyIterable() {}
-
-    public MyIterable(T[] elems) {
+    public MyIterable(final T[] elems) {
         // can i use lamdas yet?
         // this(elems, T -> true);
         this(elems, new Predicate<T>(){
             @Override
-            public boolean test(T elem) {
+            public boolean test(final T elem) {
                 return true;
             }
         });
     }
 
-    public MyIterable(T[] elems, Predicate<T> predicate) {
-        data = new LinkedList<>(Arrays.asList(elems));
+    public MyIterable(final T[] elems, final Predicate<T> predicate) {
+        data = Arrays.copyOf(elems, elems.length);
         pred = predicate;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setIterationPolicy(Predicate<T> filter) {
+    public void setIterationPolicy(final Predicate<T> filter) {
         pred = filter;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterator<T> iterator() {
         return new InnerIterator();
     }
 
-    @SuppressWarnings("unused")
     private class InnerIterator implements Iterator<T> {
 
         /**
-         * points to the element that will be returned by next()
+         * points to the element that will be returned by {@code next()}
          */
         private int pointer = 0;
 
         private void advancePointer() {
-            while (pointer < data.size() && !pred.test(data.get(pointer))) {
+            while (pointer < data.length && !pred.test(data[pointer])) {
                 pointer++;
             }
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public boolean hasNext() {
             this.advancePointer();
-            return this.pointer < data.size();
+            return this.pointer < data.length;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public T next() {
             if (!this.hasNext()) {
                 throw new NoSuchElementException();
             }
-            return data.get(pointer++);            
+            return data[pointer++];            
         }
     }   
 }
